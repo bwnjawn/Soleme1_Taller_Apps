bcbb3ad1cd7334292695202f27cd8c0c247ed6c9
Bruno Sanhueza <sanhuezamacks4@gmail.com>
2026-04-06 01:18:56 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,3 +10,11 @@ def test_get_time():
     response = client.get('/time')
     assert response.status_code == 200
     assert 'current_time' in response.json()
+    
+def test_not_found():
+    """
+    Verifica que la API maneje correctamente rutas que no existen.
+    """
+    response = client.get("/invalid-route")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}

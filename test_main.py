031ada3a3330c637bbaf1f6370fb92c83e517f55
Bruno Sanhueza <sanhuezamacks4@gmail.com>
2026-04-06 00:55:05 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 
 def test_get_time():
-    # TestClient usa httpx internamente para esta llamada
+    
     response = client.get('/time')
     assert response.status_code == 200
     assert 'current_time' in response.json()

7066b8e09ca92877b0dd896ee19afe0ac4239739
Benjamin Ojeda <benjaminojedaval@gmail.com>
2026-04-06 01:27:04 -0400

    test cambios

--- test_main.py
+++ test_main.py
@@ -10,11 +10,9 @@ def test_get_time():
     response = client.get('/time')
     assert response.status_code == 200
     assert 'current_time' in response.json()
-    
+
+
 def test_not_found():
-    """
-    Verifica que la API maneje correctamente rutas que no existen.
-    """
-    response = client.get("/invalid-route")
+    response = client.get('/invalid-route')
     assert response.status_code == 404
-    assert response.json() == {"detail": "Not Found"}
+    assert response.json() == {'detail': 'Not Found'}
